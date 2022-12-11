b47babe2c96542827bd7f0227f46b667e07f5c1a
prasanthk <prasanthk@microsoft.com>
2022-12-11 09:13:28 +0530

    aoc5 part 2

--- src/bin/aoc5.rs
+++ src/bin/aoc5.rs
@@ -38,6 +38,14 @@ impl Board {
         })
     }
 
+    fn move_some_retain_order(&mut self, many: usize, from: usize, to: usize) {
+        let pos = self.stacks[to].len();
+        (0..many).for_each(|_| {
+            let out = self.stacks[from].pop().unwrap();
+            self.stacks[to].insert(pos, out);
+        })
+    }
+
     fn print_top(&self) -> String {
         self.stacks.iter().map(|x| x.last()).flatten().collect()
     }
@@ -45,43 +53,52 @@ impl Board {
 
 fn first(filename: &str) -> Result<(), Box<dyn Error>> {
     let mut phase = Phase::Input;
-    let board = BufReader::new(OpenOptions::new().read(true).open(filename)?)
+    let (board_9000, board_9001) = BufReader::new(OpenOptions::new().read(true).open(filename)?)
         .lines()
         .flatten()
-        .fold(Board::new(), |mut board, line| {
-            if line == "" {
-                phase = Phase::Action;
-                board
-            } else {
-                match phase {
-                    Phase::Action => {
-                        let action_data: Vec<usize> = line
-                            .split(' ')
-                            .skip(1)
-                            .step_by(2)
-                            .map(str::parse)
-                            .flatten()
-                            .collect();
-                        let (many, from, to) =
-                            (action_data[0], action_data[1] - 1, action_data[2] - 1);
-                        board.move_some(many, from, to);
+        .fold(
+            (Board::new(), Board::new()),
+            |(mut board_9000, mut board_9001), line| {
+                if line == "" {
+                    phase = Phase::Action;
+                    (board_9000, board_9001)
+                } else {
+                    match phase {
+                        Phase::Action => {
+                            let action_data: Vec<usize> = line
+                                .split(' ')
+                                .skip(1)
+                                .step_by(2)
+                                .map(str::parse)
+                                .flatten()
+                                .collect();
+                            let (many, from, to) =
+                                (action_data[0], action_data[1] - 1, action_data[2] - 1);
+                            board_9000.move_some(many, from, to);
+                            board_9001.move_some_retain_order(many, from, to);
+                        }
+                        Phase::Input => {
+                            let mut line = line;
+                            line.push(' ');
+                            line.chars().array_chunks::<4>().enumerate().for_each(
+                                |(index, [bracket, char, _, _])| {
+                                    if char != ' ' && bracket == '[' {
+                                        board_9000.insert(index, char);
+                                        board_9001.insert(index, char);
+                                    }
+                                },
+                            );
+                        }
                     }
-                    Phase::Input => {
-                        let mut line = line;
-                        line.push(' ');
-                        line.chars().array_chunks::<4>().enumerate().for_each(
-                            |(index, [bracket, char, _, _])| {
-                                if char != ' ' && bracket == '[' {
-                                    board.insert(index, char);
-                                }
-                            },
-                        );
-                    }
+                    (board_9000, board_9001)
                 }
-                board
-            }
-        });
-    println!("for file {filename} top view is {:?}", board.print_top());
+            },
+        );
+    println!(
+        "for file {filename} \n\ttop view is of 9000 is {}\n\ttop view of 9001 is {}",
+        board_9000.print_top(),
+        board_9001.print_top()
+    );
     Ok(())
 }
 
